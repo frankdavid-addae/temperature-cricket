b689f9000c263cd0271e2335859808bae5c63286
Frank David Addae <frankdavid.addae@gmail.com>
2021-09-28 11:51:14 +0000

    completed

--- app/src/main/java/com/example/temperaturecricket/MainActivity.java
+++ app/src/main/java/com/example/temperaturecricket/MainActivity.java
@@ -9,11 +9,14 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.text.DecimalFormat;
+
 public class MainActivity extends AppCompatActivity {
 
     EditText etChirps;
     Button btnCalcTemp;
     TextView tvResults;
+    DecimalFormat formatter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,12 +28,19 @@ public class MainActivity extends AppCompatActivity {
         tvResults = findViewById(R.id.tvResults);
 
         tvResults.setVisibility(View.GONE);
+        formatter = new DecimalFormat("#0.0");
 
         btnCalcTemp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 if (etChirps.getText().toString().isEmpty()) {
                     Toast.makeText(MainActivity.this, "Please enter number of chirps", Toast.LENGTH_SHORT).show();
+                } else {
+                    int chirps = Integer.parseInt(etChirps.getText().toString().trim());
+                    double temperature = (chirps / 3.0) + 4;
+                    String results = "The approximate temperature outside is " + formatter.format(temperature) + " degrees Celsius.";
+                    tvResults.setText(results);
+                    tvResults.setVisibility(View.VISIBLE);
                 }
             }
         });
